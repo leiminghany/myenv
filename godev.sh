957d2690bbbbe0eb66a0a4cb665e956ba6ff0987
leiminghuan <leiminghuan@126.com>
2021-03-28 20:51:28 +0800

    add vscode

--- godev.sh
+++ godev.sh
@@ -1,1 +1,1 @@
-docker run -it --rm -v `pwd`:/root/work --name godev leiminghany/godev:0.1
+docker run --name godev -itd -v `pwd`:/root/work -p 8081:8080 --security-opt=seccomp:unconfined leiminghany/godev:0.2-2-ga5a2b23
